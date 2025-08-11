0720e39c4f3c1b0af3b6e45a8e75eea33db514f6
liana <lylaent@gmail.com>
2025-08-11 12:59:38 +0330

    add pinia to main.ts

--- shop-project/src/main.ts
+++ shop-project/src/main.ts
@@ -1,11 +1,19 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
 import './style.css'
 import router from './router/index.ts'
 import App from './App.vue'
+
+
 const app = createApp(App)
+const pinia = createPinia()
+
+app.use(pinia)
 app.use(router)
 
 app.mount('#app')
+
 
 
 
