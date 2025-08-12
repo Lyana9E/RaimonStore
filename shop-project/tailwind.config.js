314adab4f6e7c82e2dceba8718c004ddd48ee505
liana <lylaent@gmail.com>
2025-08-12 10:15:48 +0330

    set color for tailwind.config

---
+++ shop-project/tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'primary': '#22223b',
+        'second':'#9a8c98',
+        'third': '#c9ada7',
+        'gray': '#4a4e69',
+        'light':'#f2e9e4'
+      }
+    },
+  },
+  plugins: [],
+}
+
