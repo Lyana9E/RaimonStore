c5896d3f4cb25c2bea302aa5c0afc8c81f6d427b
liana <lylaent@gmail.com>
2025-08-12 13:41:58 +0330

    set mock for second slider

---
+++ shop-project/src/mocks/FeaturedProducts.ts
@@ -0,0 +1,38 @@
+export interface Product {
+    id: number;
+    title: string;
+    image: string;
+}
+
+export const featuredProducts: Product[] = [
+    {
+        id: 1,
+        title: 'تیشرت نخی ساده',
+        image: '/HomePic/1.jpg',
+    },
+    {
+        id: 2,
+        title: 'شلوار جین تیره',
+        image: '/HomePic/2.jpg',
+    },
+    {
+        id: 3,
+        title: 'کاپشن چرم مشکی',
+        image: '/HomePic/3.jpg',
+    },
+    {
+        id: 4,
+        title: 'لباس ورزشی',
+        image: '/HomePic/4.jpg',
+    },
+    {
+        id: 5,
+        title: 'پیراهن مردانه',
+        image: '/HomePic/5.jpg',
+    },
+    {
+        id: 6,
+        title: 'دامن پلیسه‌دار',
+        image: '/HomePic/6.jpg',
+    },
+];
