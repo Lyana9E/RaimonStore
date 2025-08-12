23712a6b0edfbb20ae98426cf6c28eb3ff5d4255
liana <lylaent@gmail.com>
2025-08-12 13:22:49 +0330

    add first section to home page ( banner section )

--- shop-project/src/pages/Home.vue
+++ shop-project/src/pages/Home.vue
@@ -1,7 +1,35 @@
+
+
+
 <template>
-  <h1>Home</h1>
-  <p class="text-red-500">به سایت ما خوش آمدید</p>
+  <div class="p-8 font-sans rtl">
+    <h1 class="text-primary mb-6 text-center text-xl font-semibold">
+      ALLEY
+    </h1>
+
+    <section class="mb-10 rounded-xl overflow-hidden shadow-lg">
+      <CarouselWrapper :autoplay="3000" :wrapAround="true" :transition="500" dir="rtl">
+        <template #slides>
+          <Slide v-for="slide in bannerSlides" :key="slide.id">
+            <div class="w-full h-96">
+              <img :src="slide.image" :alt="slide.alt" class="w-full h-full object-cover"/>
+            </div>
+          </Slide>
+        </template>
+      </CarouselWrapper>
+    </section>
+
+
+  </div>
 </template>
 
 <script setup lang="ts">
+
+
+import {bannerSlides} from '../mocks/HomeBannerSlider.ts';
+import CarouselWrapper from "../components/CarouselWrapper.vue";
+import { Slide } from 'vue3-carousel';
+
+
 </script>
+
