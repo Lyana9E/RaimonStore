f31afa51355e2a827de766b7f452172c1f0e908e
liana <lylaent@gmail.com>
2025-08-12 12:57:01 +0330

    set mock data for banner slider in home page

---
+++ shop-project/src/mocks/HomeBannerSlider.ts
@@ -0,0 +1,14 @@
+
+interface BannerSlide {
+    id: number;
+    image: string;
+    alt?: string;
+}
+
+export const bannerSlides: BannerSlide[] = [
+    { id: 1, image: '/HomePic/1.jpg', alt: 'بنر تخفیف ویژه' },
+    { id: 2, image: '/HomePic/2.jpg', alt: 'محصولات جدید رسید' },
+    { id: 3, image: '/HomePic/3.jpg', alt: 'پیشنهادهای شگفت‌انگیز' },
+    { id: 4, image: '/HomePic/4.jpg', alt: 'پیشنهادهای شگفت‌انگیز' },
+    { id: 5, image: '/HomePic/5.jpg', alt: 'پیشنهادهای شگفت‌انگیز' },
+];
