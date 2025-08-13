d6c82c848005741edb274b0683511bc18753cb6f
liana <lylaent@gmail.com>
2025-08-13 15:19:27 +0330

    create product card

---
+++ shop-project/src/components/ProductCard.vue
@@ -0,0 +1,39 @@
+<template>
+  <router-link :to="{ name: 'ProductDetails', params: { id: product.id } }" class="block">
+    <div class="bg-white rounded-lg shadow-md overflow-hidden transform hover:scale-105 transition-all duration-300">
+      <img :src="product.image" :alt="product.title" class="w-full h-48 object-cover" />
+      <div class="p-4">
+        <h3 class="text-lg text-gray font-semibold truncate">{{ product.title }}</h3>
+        <p class="text-primary text-sm mt-1 mb-2">{{ product.category }}</p>
+        <div class="flex items-center justify-between">
+          <span class="text-xl font-bold text-003554">${{ product.price }}</span>
+
+          <a-button
+              type="primary"
+
+              @click.prevent="handleAddToCart"
+          >
+            افزودن به سبد خرید
+          </a-button>
+        </div>
+      </div>
+    </div>
+  </router-link>
+</template>
+
+<script setup lang="ts">
+import type { Product } from '../types';
+import { Button as AButton } from 'ant-design-vue';
+
+const props = defineProps<{
+  product: Product;
+}>();
+
+const emit = defineEmits(['add-to-cart']);
+
+const handleAddToCart = () => {
+  emit('add-to-cart', props.product);
+};
+</script>
+
+
