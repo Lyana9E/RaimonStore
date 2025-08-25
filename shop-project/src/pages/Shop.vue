7e0041a41fffd8f5a8db2ce23dcb5b793bffbd13
liana <lylaent@gmail.com>
2025-08-25 11:12:54 +0330

    edit filter box in shop page

--- shop-project/src/pages/Shop.vue
+++ shop-project/src/pages/Shop.vue
@@ -3,30 +3,34 @@
     <h1 class="text-3xl font-bold mb-6 text-center text-gray-800">فروشگاه</h1>
 
     <div class="mb-6 flex flex-col sm:flex-row items-center gap-4">
-      <a-input-search
+      <aInputSearch
           v-model:value="productStore.searchTerm"
           placeholder="جستجوی محصول..."
           size="large"
           class="w-full sm:w-1/2"
           @search="onSearch"
       />
-      <div class="flex items-center gap-2 w-full sm:w-auto">
-        <span class="text-gray-600 whitespace-nowrap">مرتب‌سازی بر اساس قیمت:</span>
-        <a-select
-            v-model:value="productStore.sortOrder"
-            class="w-full sm:w-40"
-            placeholder="انتخاب کنید"
-            allowClear
-        >
-          <a-select-option value="asc">کم به زیاد</a-select-option>
-          <a-select-option value="desc">زیاد به کم</a-select-option>
-        </a-select>
+      <div class="flex-col items-center gap-2 w-full sm:w-auto">
+        <span class="text-primary whitespace-nowrap">مرتب‌سازی بر اساس قیمت:</span>
+        <a-radio-group v-model:value="productStore.sortOrder" button-style="solid" class="flex items-center ">
+          <a-radio-button value="asc" >کم به زیاد</a-radio-button>
+          <a-radio-button value="desc">زیاد به کم</a-radio-button>
+        </a-radio-group>
+
+        <a-button v-if="productStore.sortOrder" @click="clearSortOrder" type="text" danger shape="circle" size="large">
+          <template #icon>
+            <close-outlined class="text-lg" />
+          </template>
+        </a-button>
       </div>
     </div>
 
-    <a-spin :spinning="productStore.isLoading" size="large" tip="در حال بارگذاری محصولات...">
+
+
+
+    <aSpin :spinning="productStore.isLoading" size="large" tip="در حال بارگذاری محصولات...">
       <div v-if="productStore.error">
-        <a-alert
+        <aAlert
             message="خطا"
             :description="productStore.error"
             type="error"
@@ -35,7 +39,7 @@
       </div>
 
       <div v-else-if="productStore.filteredAndSortedProducts.length === 0" class="mt-8 text-center text-gray-500">
-        <a-empty description="محصولی یافت نشد." />
+        <aEmpty description="محصولی یافت نشد." />
       </div>
 
       <div v-else class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 mt-8">
@@ -46,7 +50,7 @@
             @add-to-cart="handleAddToCart"
         />
       </div>
-    </a-spin>
+    </aSpin>
   </div>
 </template>
 
@@ -59,6 +63,9 @@ import type { Product } from '../types';
 import {
   notification
 } from 'ant-design-vue';
+import { CloseOutlined } from '@ant-design/icons-vue';
+
+
 
 const productStore = useProductStore();
 const cartStore = useCartStore();
@@ -80,5 +87,9 @@ const handleAddToCart = (product: Product) => {
 
 const onSearch = (value: string) => {
   console.log('Searching for:', value);
+};
+
+const clearSortOrder = () => {
+  productStore.sortOrder = null;
 };
 </script>
